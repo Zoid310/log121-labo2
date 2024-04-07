d3c1f29bb61eb916ea58386b6bd7e3544b6498ff
Rvnivv <Raniia30@hotmail.com>
2024-04-06 22:13:00 -0400

    Jai un peu implemente views

--- src/views/PerspectiveView.java
+++ src/views/PerspectiveView.java
@@ -2,28 +2,51 @@ package views;
 
 import controller.PerspectiveController;
 import models.PerspectiveModel;
-import observer.*;
+import observer.Observer;
+import observer.Subject;
 
-public class PerspectiveView implements Observer {
-	
-	private PerspectiveModel model;
-	private PerspectiveController controller;
-	
-	public PerspectiveView(PerspectiveModel modelInstance) {
+import javax.swing.*;
+import java.awt.*;
+
+public class PerspectiveView extends JPanel implements Observer {
+    
+    private PerspectiveModel model;
+    private PerspectiveController controller;
+    
+    public PerspectiveView(PerspectiveModel modelInstance) {
         this.model = modelInstance;
         this.controller = new PerspectiveController();
-	}
-	
-	public void loadImage() {
-		// Calls the controller to load the image
-	}
-	
-	public void display() {
-		// TODO
-	}
-	
-	public void update(Subject subject) {
-		// TODO
-	}
-	
+        this.model.add(this); // S'abonner aux mises a jour du modele
+        
+        setLayout(new BorderLayout());
+    }
+    
+    public void loadImage() {
+
+		//TODO
+        
+    }
+    
+    @Override
+    public void update(Subject subject) {
+        // appelle quand modele notifie un changement
+        display(); 
+    }
+    
+    public void display() {
+        
+        this.removeAll(); 
+
+        if (model.getImage() != null) {
+            // afficher limage
+            JLabel label = new JLabel(new ImageIcon(model.getImage().getPath()));
+            add(label, BorderLayout.CENTER);
+        } else {
+            // message si aucune image nest chargee
+            add(new JLabel("Aucune perspective disponible"), BorderLayout.CENTER);
+        }
+        
+        revalidate();
+        repaint();
+    }
 }
--- src/views/ThumbnailView.java
+++ src/views/ThumbnailView.java
@@ -3,23 +3,39 @@ package views;
 import models.ThumbnailModel;
 import observer.*;
 
-public class ThumbnailView implements Observer {
+public class ThumbnailView extends JPanel implements Observer {
 	
 	private ThumbnailModel model;
 	
 	public ThumbnailView(ThumbnailModel modelInstance) {
 		this.model = modelInstance;
+		model.add(this);
+
+		setLayout(new BorderLayout());
+		imageLabel = new JLabel();
+		add(imageLabel, BorderLayout.CENTRE);
 	}
 	
 	public void loadImage() {
+		
 		//TODO: Calls the controller to load the thumbnail or something else
+		
 	}
 	
 	public void display() {
-		//TODO
+
+		if (model.getImageIcon() != null) {
+            imageLabel.setIcon(model.getImageIcon());
+        } else {
+            imageLabel.setText("Aucune image disponible");
+        }
+        revalidate();
+        repaint();
+    }
 	}
 	
 	public void update(Subject subject) {
-		//TODO
+		display(); // mise a jour de laffichage
+		
 	}
 }
