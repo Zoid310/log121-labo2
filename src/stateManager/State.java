5e0742682abbb2b92c48696ff0c7ca04d9cd2c77
jouellet310 <justin_ouellet@hotmail.com>
2024-04-06 21:41:42 -0400

    Change SaveManager for StateManager, Add stuff in state.

--- src/stateManager/State.java
+++ src/stateManager/State.java
@@ -1,14 +1,20 @@
 package stateManager;
 
+import java.io.Serializable;
+
 import views.*;
 
-public class State {
+public class State implements Serializable{
 	
 	private ThumbnailView thumbnail;
 	private PerspectiveView perspective1;
 	private PerspectiveView perspective2;
 	
-	public State() {
+	public State(ThumbnailView cThumbnail, PerspectiveView cPerspective1, PerspectiveView cPerspective2) {
+		
+        this.thumbnail = cThumbnail;
+        this.perspective1 = cPerspective1;
+        this.perspective2 = cPerspective2;
 		
 	}
 }
---
+++ src/stateManager/StateManager.java
@@ -0,0 +1,22 @@
+package stateManager;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+public class StateManager {
+	
+	public void saveStateToFile(String fileName, State state) throws IOException {
+		// TODO
+	}
+	
+	public State loadStateFromFile(String fileName) {
+		// TODO
+
+		return null;
+	}
+
+	private Serializable serializableState(State state) {
+		// TODO
+		return null;
+	}
+}
