51cbbd7f3b9bd33233e1f6bf29bd982d08345d9c
jouellet310 <justin_ouellet@hotmail.com>
2024-04-06 22:10:16 -0400

    Added saveStateToFile and loadStateFromFile in StateManager.

--- src/stateManager/State.java
+++ src/stateManager/State.java
@@ -1,20 +1,32 @@
 package stateManager;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 import views.*;
 
-public class State implements Serializable{
-	
+public class State implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	private ThumbnailView thumbnail;
 	private PerspectiveView perspective1;
 	private PerspectiveView perspective2;
-	
+
+	private void writeObject(ObjectOutputStream oos) throws IOException {
+		oos.defaultWriteObject();
+	}
+
+	private void readObject(ObjectInputStream ois) throws ClassNotFoundException, IOException {
+        ois.defaultReadObject();
+    }
+
 	public State(ThumbnailView cThumbnail, PerspectiveView cPerspective1, PerspectiveView cPerspective2) {
-		
-        this.thumbnail = cThumbnail;
-        this.perspective1 = cPerspective1;
-        this.perspective2 = cPerspective2;
-		
+
+		this.thumbnail = cThumbnail;
+		this.perspective1 = cPerspective1;
+		this.perspective2 = cPerspective2;
+
 	}
 }
